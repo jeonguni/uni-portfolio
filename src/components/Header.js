267a55c6c1b63f0cfd353274ac3e2128e85965a4
ejy55n <ejy55n2@gmail.com>
2023-10-25 17:19:09 +0900

    style: profile작업

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
+import { useState, useEffect } from 'react';
+
 const Header = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const useScroll = () => {
+    setScrollPosition(document.documentElement.scrollTop);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', useScroll);
+    console.log(scrollPosition);
+  });
   return (
-    <header>
+    <header className={scrollPosition > 50 ? 'on' : ''}>
       <h1>Uni's</h1>
-      <ul>
+      <ul className="header-list">
         <li>
           <a href="#home">home</a>
         </li>
